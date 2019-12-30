dfa568feccbc7a8282cec624b36f70b531d422f1
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2019-12-29 23:00:24 -0500

    Fix nvm on xcode8.3 image

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,10 @@ set -o pipefail
 
 if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
   brew install nvm
+  mkdir ~/.nvm
+  touch ~/.bashrc
+  echo "export NVM_DIR=\"$HOME/.nvm\"" >> ~/.bashrc
+  echo "[ -s \"/usr/local/opt/nvm/nvm.sh\" ] && . \"/usr/local/opt/nvm/nvm.sh\"" >> ~/.bashrc
 else
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 fi
