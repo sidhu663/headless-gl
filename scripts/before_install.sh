a86915188e8a0369bc8c7f111836006d771e9146
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2019-12-30 00:36:29 -0500

    Make sure all macOS headers are installed

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,6 +2,8 @@
 set -o pipefail
 
 if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+  sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+
   brew install nvm
   mkdir ~/.nvm
   touch ~/.bashrc
